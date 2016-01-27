0a83391b1f680a50559d4b4b09e3b890fc21cfa5
songshu189 <songshu.yu@yahoo.com>
2016-01-27 18:55:37 -0500

    feat: Add and remove books from the library

--- js/views/book.js
+++ js/views/book.js
@@ -7,6 +7,18 @@ app.BookView = Backbone.View.extend({
     className: 'bookContainer',
     template: _.template( $( '#bookTemplate' ).html() ),
 
+    events: {
+        'click .delete': 'deleteBook'
+    },
+
+    deleteBook: function() {
+        //Delete model
+        this.model.destroy();
+
+        //Delete view
+        this.remove();
+    },
+
     render: function() {
         //this.el is what we defined in tagName. use $el to get access to jQuery html() function
         this.$el.html( this.template( this.model.attributes ) );
--- js/views/library.js
+++ js/views/library.js
@@ -12,6 +12,30 @@ app.LibraryView = Backbone.View.extend({
         this.listenTo( this.collection, 'add', this.renderBook );
     },
 
+    events:{
+        'click #add':'addBook'
+    },
+
+    addBook: function( e ) {
+        e.preventDefault();
+
+        var formData = {};
+
+        $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
+            if( $( el ).val() != '' )
+            {
+                if( el.id === 'coverImage') {
+                    formData[ el.id ] = 'img/' + $( el ).val().replace(/^C:\\fakepath\\/i, '');
+                } else {
+                    formData[ el.id ] = $( el ).val();
+                }
+                console.log(formData[ el.id ] );
+            }
+        });
+
+        this.collection.add( new app.Book( formData ) );
+    },
+
     // render library by rendering each book in its collection
     render: function() {
         this.collection.each(function( item ) {
@@ -27,5 +51,4 @@ app.LibraryView = Backbone.View.extend({
         });
         this.$el.append( bookView.render().el );
     }
-
 });
